7b72d69754615d7e7768821356c258aa5bf24169
John Bradley <johnbradley2008@gmail.com>
2023-04-21 14:51:03 -0400

    Configure global react settings
    Configures dev and prod settings for where to find the API.
    Removes unused nextjs api files.
    Simplifies global css configuration.
    Disables image optimization since we are using export.

--- andromeda-ui/next.config.js
+++ andromeda-ui/next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
-module.exports = nextConfig
+module.exports = (phase, { defaultConfig }) => {
+  let apiURL = "api";
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    apiURL = "http://127.0.0.1:5000/api";
+  }
+  return {
+    images: {
+      unoptimized: true
+    },
+    reactStrictMode: true,
+    publicRuntimeConfig: {
+      apiURL: apiURL,
+    },
+    output: "export",
+  };
+};
