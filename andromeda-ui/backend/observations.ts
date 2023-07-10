48d0ae52dca60ecfde869f2bc8b305c1b8ecb62f
egrace479 <e.campolongo479@gmail.com>
2023-07-10 14:30:10 -0400

    adds checkboxes for satellite and landcover data retrieval

--- andromeda-ui/backend/observations.ts
+++ andromeda-ui/backend/observations.ts
@@ -1,7 +1,7 @@
 import { fetch_json, apiURL } from "./util";
 
-export async function fetchObservations(iNatUser: string) {
-    const url = makeObservationURL(iNatUser, "json");
+export async function fetchObservations(iNatUser: string, addSatCSV: boolean, addLandCover: boolean) {
+    const url = makeObservationURL(iNatUser, addSatCSV, addLandCover, "json");
     return await fetch_json(url, {
       method: "GET",
       headers: {
@@ -10,10 +10,12 @@ export async function fetchObservations(iNatUser: string) {
     });
 }
 
-export function makeObservationURL(iNatUser: string, format: "json" | "csv") {
+export function makeObservationURL(iNatUser: string, addSatCSV: boolean, addLandCover: boolean, format: "json" | "csv") {
   let base_url = "/inaturalist/" + iNatUser;
   if (format) {
     base_url += "?format=" + format;
   }
+  base_url += "&add_sat_csv_data=" + addSatCSV;
+  base_url += "&add_land_api_data=" + addLandCover;
   return apiURL(base_url);
 }
--- andromeda-ui/components/FetchDataForm.tsx
+++ andromeda-ui/components/FetchDataForm.tsx
@@ -1,23 +1,40 @@
 import ColoredButton from "../components/ColoredButton";
+import SimpleSelect from "./SimpleSelect";
 
 interface FetchDataFormProps {
     iNatUser: string | undefined;
     setINatUser: (user: string) => void;
     disableFetchButton: boolean;
     onClickFetch: () => void;
+    addSatCSVData: boolean;
+    setAddSatCSVData: (add: boolean) => void;
+    addLandCover: boolean;
+    setAddLandCover: (add: boolean) => void;
 }
 
 export default function FetchDataForm(props: FetchDataFormProps) {
-    const {iNatUser, setINatUser, disableFetchButton, onClickFetch} = props;
+    const {iNatUser, setINatUser, disableFetchButton, onClickFetch, addSatCSVData, setAddSatCSVData, addLandCover, setAddLandCover} = props;
+    function onChangeCSV(event: any){
+        setAddSatCSVData(event.target.checked)
+    }
+    function onChangeLand(event: any){
+        setAddLandCover(event.target.checked)
+    }
     return <>
         <p className="max-w-2xl text-sm my-2">
             Find iNaturalist observations to create a CSV file that can be visualized in Andromeda.
         </p>
         <div className="py-1">
             <label className="mr-2 text-md font-medium" htmlFor="iNatUser">iNaturalist Username:</label>
-            <input className="border rounded p-1" type="text" id="iNatUser" name="iNatUser" required
+            <input className="mr-4 border rounded p-1" type="text" id="iNatUser" name="iNatUser" required
                 value={iNatUser}
                 onChange={(e: any) => setINatUser(e.target.value)} />
+        
+            <label className="mr-2 text-md font-medium" htmlFor="AddSatCSV">RGB Satellite Data</label>
+            <input className="mr-4 accent-cyan-600" type="checkbox" id="AddSatCSV" checked={addSatCSVData} onChange={onChangeCSV}></input>
+    
+            <label className="mr-2 text-md font-medium" htmlFor="AddLandCover">Landcover Data</label>
+            <input className="accent-cyan-600" type="checkbox" id="AddLandCover" checked={addLandCover} onChange={onChangeLand}></input>
         </div>
         <div className="my-2">
             <ColoredButton disabled={disableFetchButton} label="Fetch Observations" color="blue" onClick={onClickFetch} />
