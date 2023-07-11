c6092f7d940cfbc6a5c3a7e9ff16a16c56d5d4b4
John Bradley <johnbradley2008@gmail.com>
2023-07-11 16:03:45 -0400

    Show all observation columns
    Changes UI to show all columns in observation data
    Makes images smaller so everything is easier to see.
    
    Fixes #47

--- andromeda-ui/components/ObservationTable.tsx
+++ andromeda-ui/components/ObservationTable.tsx
@@ -7,18 +7,39 @@ interface ObservationTableProps {
 }
 
 const TH_CLASSNAME = "p-2 border text-slate-500 font-semi-bold text-sm"
-const TD_CLASSNAME = "p-1 border text-slate-500 text-sm"
+const TD_CLASSNAME = "p-1 border text-slate-500 text-sm whitespace-nowrap text-ellipsis"
+const KNOWN_COLUMNS = new Set([
+    "Image_Label",
+    "Image_Link",
+    "Date",
+    "Time",
+    "Lat",
+    "Long",
+    "Species",
+    "Place",
+    "Seconds",
+    "User"
+]);
 
 export default function ObservationTable(props: ObservationTableProps) {
     const { observations, maxObs, iNatUser } = props;
     const showing = Math.min(observations.length, maxObs);
+    let extraColumns: string[] = [];
+    if (observations.length > 0) {
+        extraColumns = Object.keys(observations[0]).filter((x) => !KNOWN_COLUMNS.has(x))
+    }
     const rows = observations.slice(0, maxObs).map((x) => {
+        const extraColumnValues = extraColumns.map(colname => {
+            return <td key={colname + "_" + x.Image_Label} className={TD_CLASSNAME}>
+                {x[colname]}
+            </td>;
+        })
         return <tr key={x.Image_Label}>
-            <td className={TD_CLASSNAME}>{x.Image_Label}</td>
+            <td className={TD_CLASSNAME + " sticky left-0 bg-white"}>{x.Image_Label}</td>
             <td className={TD_CLASSNAME}>
                 <Image
-                    width="64"
-                    height="64"
+                    width={50}
+                    height={50}
                     src={x.Image_Link}
                     alt={x.Image_Label}
                     title={x.Image_Link} />
@@ -47,31 +68,38 @@ export default function ObservationTable(props: ObservationTableProps) {
             <td className={TD_CLASSNAME}>
                 {x.User}
             </td>
+            {extraColumnValues}
         </tr>
     });
+    const extraColumnHeaders = extraColumns.map(colname => {
+        return <th key={colname} className={TH_CLASSNAME}>{colname}</th>;
+    })
     return <>
         <div className="my-1">
             <span className="text-lg flex-auto">Observations by {iNatUser}</span>
         </div>
-        <table className="border-collapse border">
-            <thead className="bg-slate-50 border-slate-400">
-                <tr>
-                    <th className={TH_CLASSNAME}>Image_Label</th>
-                    <th className={TH_CLASSNAME}>Image_Link</th>
-                    <th className={TH_CLASSNAME}>Date</th>
-                    <th className={TH_CLASSNAME}>Time</th>
-                    <th className={TH_CLASSNAME}>Lat</th>
-                    <th className={TH_CLASSNAME}>Long</th>
-                    <th className={TH_CLASSNAME}>Species</th>
-                    <th className={TH_CLASSNAME}>Place</th>
-                    <th className={TH_CLASSNAME}>Seconds</th>
-                    <th className={TH_CLASSNAME}>User</th>
-                </tr>
-            </thead>
-            <tbody>
-                {rows}
-            </tbody>
-        </table>
+        <div className="overflow-auto">
+            <table className="border-collapse border table-auto">
+                <thead className="bg-slate-50 border-slate-400">
+                    <tr>
+                        <th className={TH_CLASSNAME+ " sticky left-0 bg-slate-50"}>Image_Label</th>
+                        <th className={TH_CLASSNAME}>Image_Link</th>
+                        <th className={TH_CLASSNAME}>Date</th>
+                        <th className={TH_CLASSNAME}>Time</th>
+                        <th className={TH_CLASSNAME}>Lat</th>
+                        <th className={TH_CLASSNAME}>Long</th>
+                        <th className={TH_CLASSNAME}>Species</th>
+                        <th className={TH_CLASSNAME}>Place</th>
+                        <th className={TH_CLASSNAME}>Seconds</th>
+                        <th className={TH_CLASSNAME}>User</th>
+                        {extraColumnHeaders}
+                    </tr>
+                </thead>
+                <tbody>
+                    {rows}
+                </tbody>
+            </table>
+        </div>
         <span className="text-sm">
             Showing {showing} rows ({observations.length} in total)
         </span>
